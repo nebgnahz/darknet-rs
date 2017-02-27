e9105dd1fd6122387ee50f90d403e1f576dde347
Ben Zhang <benzh@cs.berkeley.edu>
2017-02-26 22:17:29 -0800

    feat: add custom wrapper

--- build.rs
+++ build.rs
@@ -6,17 +6,22 @@ use std::path::PathBuf;
 fn main() {
     let bindings = bindgen::Builder::default()
         .no_unstable_rust()
-        .header("wrapper.h")
+        .header("darknet-sys/src/wrapper.h")
         .whitelist_recursively(true)
-        .whitelisted_type("list")
-        .whitelisted_function("read_data_cfg")
-        .whitelisted_function("option_find_str")
+        .whitelisted_type("Size")
+        .whitelisted_type("Rect")
+        .whitelisted_type("Detections")
+        .whitelisted_type("Darknet")
+        .whitelisted_type("InputImage")
+        .whitelisted_function("darknet_new")
+        .whitelisted_function("darknet_drop")
+        .whitelisted_function("darknet_size")
+        .whitelisted_function("darknet_detect")
         .link("darknet-sys/darknet.so")
         .generate()
         .expect("Unable to generate bindings");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
+    bindings.write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
 }
