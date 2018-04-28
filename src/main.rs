06bb7c8c01e9a3560f4b126ec15e04b81e0f92fa
Ben Zhang <benzh@cs.berkeley.edu>
2018-04-28 13:32:44 -0700

    change main to detect objects

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,20 @@ fn main() {
 
 fn run() -> Result<(), Error> {
     std::env::set_current_dir("darknet-sys").unwrap();
-    // let network = Network::new("cfg/yolov2-tiny.cfg", "yolov2-tiny.weights")?;
-    // let meta = Meta::new("cfg/coco.data")?;
-    let image = Image::load("data/dog.jpg")?;
-    // let dets = simple_detect(&network, &meta, &image);
-    // for d in &dets {
-    //     image.draw_box(d, 1, 1.0, 0.0, 0.0);
-    // }
-    // println!("{:?}", dets);
-    image.save("dog2")?;
-    image.save_jpg("dog2")?;
+
+    let network = Network::new("cfg/yolov2-tiny.cfg", "yolov2-tiny.weights")?;
+    let meta = Meta::new("cfg/coco.data")?;
+    let mut image = Image::load("data/dog.jpg")?;
+    let dets = simple_detect(&network, &meta, &image);
+    for d in &dets {
+        image.draw_box(d, 1, 1.0, 0.0, 0.0);
+    }
+    println!("{:?}", dets);
 
     let data = image.encode_jpg();
     {
-        let mut buffer = ::std::fs::File::create("foo.jpg")?;
+        let mut buffer = ::std::fs::File::create("prediction.jpg")?;
         buffer.write_all(&data)?;
     }
-
-    let decoded = Image::decode_jpg(&data);
-    decoded.save_jpg("decoded")?;
     Ok(())
 }
