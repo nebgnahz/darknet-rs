5ed19a555ef1986bdc56d73022ce39423507ec08
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-29 21:44:30 +0000

    Deploy nebgnahz/darknet-rs to github.com/nebgnahz/darknet-rs.git:gh-pages

---
+++ implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["darknet"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"darknet/ffi/struct.metadata.html\" title=\"struct darknet::ffi::metadata\">metadata</a>&gt; for <a class=\"struct\" href=\"darknet/struct.Meta.html\" title=\"struct darknet::Meta\">Meta</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
