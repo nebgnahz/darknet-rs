4b0709ec96ed74614361b2e3122b8d95b71833de
Ben Zhang <benzh@cs.berkeley.edu>
2018-04-29 14:39:58 -0700

    cuda search lib64 as well

--- build.rs
+++ build.rs
@@ -32,6 +32,7 @@ fn main() {
 
     if cfg!(feature = "cuda") {
         println!("cargo:rustc-link-search=native=/usr/local/cuda/lib");
+        println!("cargo:rustc-link-search=native=/usr/local/cuda/lib64");
         println!("cargo:rustc-link-lib=cuda");
         println!("cargo:rustc-link-lib=cudart");
         println!("cargo:rustc-link-lib=cublas");
